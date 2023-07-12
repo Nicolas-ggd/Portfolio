c5c26eb7aaf49e2a988d4e059e75c10938d80947
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-12 17:44:17 +0400

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { Switcher } from "../../utils/Switcher";
 
 export const Header = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const location = useLocation();
 
   const handleMobileMenuToggle = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
+
+  useEffect(() => {
+    setMobileMenuOpen(false);
+  }, [location]);
 
   return (
     <div className="bg-white border-gray-200 px-4 transition duration-300 lg:px-6 py-6 dark:bg-gray-800">
@@ -22,7 +27,19 @@ export const Header = () => {
           >
             <span className="sr-only">Open main menu</span>
             <svg
-              className="w-6 h-6"
+              className={`${isMobileMenuOpen ? "w-6 h-6" : "hidden"} z-50 absolute right-5 animate duration-300`}
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 11.414l4.293 4.293a1 1 0 001.414-1.414L11.414 10l4.293-4.293a1 1 0 00-1.414-1.414L10 8.586 5.707 4.293a1 1 0 00-1.414 1.414L8.586 10 4.293 14.293a1 1 0 001.414 1.414L10 11.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <svg
+              className={!isMobileMenuOpen ? "w-6 h-6" : "hidden"}
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -31,29 +48,17 @@ export const Header = () => {
                 fillRule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                 clipRule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              className="hidden w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
+              />
             </svg>
           </button>
         </div>
         <div
           className={`${
-            isMobileMenuOpen ? "block" : "hidden"
-          } justify-between items-center w-full lg:flex lg:w-auto lg:order-1`}
+            isMobileMenuOpen ? "fixed inset-0 z-999 bg-white" : "hidden"
+          } justify-between bg-gray-800 items-center w-full lg:flex lg:w-auto lg:order-1`}
           id="mobile-menu-2"
         >
-          <ul className="lg:flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+          <ul className="lg:flex p-5 flex-col mt-12 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
             <li>
               <Link
                 to="/"
@@ -80,9 +85,9 @@ export const Header = () => {
             </li>
           </ul>
         </div>
-        <div className="flex w-full justify-end items-center lg:order-3">
+        {!isMobileMenuOpen && <div className="flex w-full justify-end items-center lg:order-3">
           <Switcher />
-        </div>
+        </div>}
       </div>
     </div>
   );
