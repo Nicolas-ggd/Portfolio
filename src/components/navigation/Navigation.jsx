2b49667302ac06594e3e479ebb1b8244ea53f47c
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-18 23:44:20 +0400

    update navigation animate

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
@@ -23,19 +24,21 @@ const navigationData = [
 ];
 
 export const Navigation = () => {
+  const [active, setActive] = useState(0);
   const location = useLocation();
   const pathname = location.pathname;
 
   return (
     <div className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 dark:bg-gray-700 py-10 bg-gray-200 h-[80px] xl:h-max text-3xl backdrop-blur-sm xl:rounded-full xl: px-5">
+      <div className="flex relative w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 dark:bg-gray-700 py-10 bg-gray-200 h-[80px] xl:h-max text-3xl backdrop-blur-sm xl:rounded-full xl:px-5 px-20">
         {navigationData?.map((item, index) => {
           return (
             <Link
-              className="text-dark dark:text-white transition-all duration-200 relative flex items-center group hover:dark:text-orange-300"
+              className="text-dark dark:text-white transition-all duration-300 relative flex items-center group hover:dark:text-orange-300"
               style={{ color: pathname === item?.path ? "orange" : "" }}
               key={index}
               to={item?.path}
+              onClick={() => setActive(index)}
             >
               <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
                 <div className="bg-white relative flex text-black items-center p-[6px] rounded-[3px]">
@@ -44,7 +47,7 @@ export const Navigation = () => {
                   </div>
                 </div>
               </div>
-              {item?.icon}
+              <div className={`${active === index ? "translate-y-[-10px]" : ""}`}>{item?.icon}</div>
             </Link>
           );
         })}
