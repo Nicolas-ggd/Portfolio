d7dbaac6fab1798db18ccdd155035316d04da750
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-21 16:22:17 +0400

    update: transition bg color

--- src/components/transition/Transition.jsx
+++ src/components/transition/Transition.jsx
@@ -19,7 +19,7 @@ export const Transition = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2c2257]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#1c4b17] opacity-100"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
@@ -28,7 +28,7 @@ export const Transition = () => {
       >
       </motion.div>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#236b1d] opacity-100"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
@@ -37,7 +37,7 @@ export const Transition = () => {
       >
       </motion.div>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#34812d] opacity-100"
         variants={transitionVariants}
         initial="initial"
         animate="animate"
