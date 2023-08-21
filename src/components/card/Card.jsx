c1735c0e27b714f62337b66111939355a668f61e
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-22 03:23:24 +0400

    update: projects title

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -160,64 +160,72 @@ export const Card = () => {
 
   return (
     <div className="w-full px-5 sm:px-10 h-full transition duration-300 py-10 bg-gray-200 dark:bg-black">
-      <div className="flex flex-wrap justify-center">
-        {githubProjects.map((item, index) => {
-          return (
-            <div
-              key={index}
-              style={{ width: "100%", maxWidth: "320px" }}
-              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4"
-              data-aos="fade-up"
-            >
-              <div className="max-w-md h-80 m-auto p-6 hover:scale-110 hover:shadow-lg hover:shadow-gray-500/50 cursor-pointer transition duration-300 bg-white rounded-lg shadow dark:bg-zinc-900 dark:border-gray-700">
-                <div className="px-3">
-                  <img
-                    src={`https://nicolas-ggd.netlify.app${item?.imgSrc2}`}
-                    alt="No image"
-                    className="mx-auto"
-                  />
-                </div>
-                <div className="flex items-center pt-3">
+      <div className="flex flex-col mx-auto max-w-screen-xl px-3">
+        <div data-aos="fade-right" className="px-2 py-6">
+          <h1 className="text-5xl dark:text-white text-dark">Projects</h1>
+          <p className="py-2 text-xl dark:text-gray-500 py-4">
+            Discover projects I've worked on
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-center pt-5">
+          {githubProjects.map((item, index) => {
+            return (
+              <div
+                key={index}
+                style={{ width: "100%", maxWidth: "320px" }}
+                className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4"
+                data-aos="fade-up"
+              >
+                <div className="max-w-md h-80 m-auto p-6 hover:scale-110 hover:shadow-lg hover:shadow-gray-500/50 cursor-pointer transition duration-300 bg-white rounded-lg shadow dark:bg-zinc-900 dark:border-gray-700">
                   <div className="px-3">
-                    <a href={item?.githubLink}>
-                      <h5 className="mb-2 text-lg md:text-xl transition duration-300 tracking-tight text-gray-900 dark:text-white">
-                        {item.name}
-                      </h5>
-                    </a>
+                    <img
+                      src={`https://nicolas-ggd.netlify.app${item?.imgSrc2}`}
+                      alt="No image"
+                      className="mx-auto"
+                    />
                   </div>
-                  <div className="px-8 flex">
-                    <a
-                      className="px-2"
-                      href={item?.githubLink}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <GitHubIcon className="dark:text-white" />
-                    </a>
-                    <a
-                      className="px-2"
-                      href={item?.webUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <LaunchIcon className="dark:text-white" />
-                    </a>
+                  <div className="flex items-center pt-3">
+                    <div className="px-3">
+                      <a href={item?.githubLink}>
+                        <h5 className="mb-2 text-lg md:text-xl transition duration-300 tracking-tight text-gray-900 dark:text-white">
+                          {item.name}
+                        </h5>
+                      </a>
+                    </div>
+                    <div className="px-8 flex">
+                      <a
+                        className="px-2"
+                        href={item?.githubLink}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <GitHubIcon className="dark:text-white" />
+                      </a>
+                      <a
+                        className="px-2"
+                        href={item?.webUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <LaunchIcon className="dark:text-white" />
+                      </a>
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-wrap my-5">
-                  {item.lang.map((lang, langIndex) => (
-                    <p
-                      key={langIndex}
-                      className="border my-1 text-blue-800 text-xs px-2.5 transition duration-300 py-1.5 font-md mr-2 px-2.5 rounded dark:text-white"
-                    >
-                      {lang.name}
-                    </p>
-                  ))}
+                  <div className="flex flex-wrap my-5">
+                    {item.lang.map((lang, langIndex) => (
+                      <p
+                        key={langIndex}
+                        className="border my-1 text-blue-800 text-xs px-2.5 transition duration-300 py-1.5 font-md mr-2 px-2.5 rounded dark:text-white"
+                      >
+                        {lang.name}
+                      </p>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
